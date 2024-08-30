5d727efd5e811e5c3d060fa17f928c698fd6bce9
Vinayak Mahajan <mahajavi@amazon.com>
2024-08-30 13:35:45 +0100

    Update the Test as per new Pages that are created to support POM framework

--- src/sampleTest/simpleTest.java
+++ src/sampleTest/simpleTest.java
@@ -1,68 +1,61 @@
 package sampleTest;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import main.util.WebElementUtil;
+import main.util.DriverUtil;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import pageModel.CartPage;
+import pageModel.CheckOutPage;
 import pageModel.LoginPage;
+import pageModel.ProductListPage;
 
 import java.time.Duration;
-import java.util.List;
 
 public class simpleTest {
     public static void main(String[] args) {
 
         LoginPage loginPage = new LoginPage();
+        ProductListPage productListPage = new ProductListPage();
+        CartPage cartPage = new CartPage();
+        CheckOutPage checkOutPage = new CheckOutPage();
+        DriverUtil driverUtil = new DriverUtil();
 
-        WebElementUtil webElementUtil = new WebElementUtil();
-
-
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--disable-search-engine-choice-screen"));
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriver driver = driverUtil.getDriver("chrome");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
+        /* Navigate to The Website*/
         driver.get("https://rahulshettyacademy.com/client");
 
+        /* Login Page */
         loginPage.getUserEmail(driver).sendKeys("test199191@test.com");
         loginPage.getPassword(driver).sendKeys("Test@123");
         loginPage.getLoginButton(driver).click();
 
-        List<WebElement> arrayList = webElementUtil.getElements(".mb-3", driver);
+        /* Product List Page */
+        productListPage.clickProductButton(driver, "ZARA");
+        wait.until(ExpectedConditions.invisibilityOf(productListPage.getAnimationElement(driver)));
+        wait.until(ExpectedConditions.visibilityOf(productListPage.getSuccessMessage(driver)));
+        productListPage.getCartButton(driver).click();
 
-        arrayList.forEach(webElement -> {
-            if(webElementUtil.getElement("b", webElement).getText().contains("ZARA")){
-                webElementUtil.getElement(".card-body button:last-of-type", driver).click();
-            }
-        });
-
-        wait.until(ExpectedConditions.invisibilityOf(webElementUtil.getElement(".ng-animating", driver)));
-        wait.until(ExpectedConditions.visibilityOf(webElementUtil.getElement("#toast-container", driver)));
-
-        webElementUtil.getElement("[routerlink*='cart']", driver).click();
-
-        List<WebElement> list = webElementUtil.getElements(".cartSection h3", driver);
-        list.forEach(webElement -> {
-            if(webElement.getText().contains("ZARA")){
+        /* Add to Cart Page */
+        cartPage.getCartItems(driver).forEach(webElement -> {
+            if (webElement.getText().contains("ZARA")) {
                 Assert.assertTrue(true);
-            }
-            else {
+            } else {
                 Assert.fail();
             }
         });
-        webElementUtil.getElement(".totalRow button", driver).click();
+        cartPage.getCartButton(driver).click();
 
-        webElementUtil.getElement("[placeholder='Select Country']", driver).sendKeys("india");
-        wait.until(ExpectedConditions.visibilityOf(webElementUtil.getElement(".ta-results", driver)));
-        webElementUtil.getElement(".ta-item:nth-of-type(2)", driver).click();
-        webElementUtil.getElement(".action__submit", driver).click();
-        Assert.assertEquals(webElementUtil.getElement(".hero-primary", driver).getText(), "THANKYOU FOR THE ORDER.");
-        driver.quit();
+        /* Checkout Page */
+        checkOutPage.getCountryTextBox(driver).sendKeys("india");
+        wait.until(ExpectedConditions.visibilityOf(checkOutPage.getCountryDropDown(driver)));
+        checkOutPage.getExactCountyFromDropDown(driver).click();
+        checkOutPage.getPlaceOrderButton(driver).click();
+        Assert.assertEquals(checkOutPage.getSuccessMessage(driver).getText(), "THANKYOU FOR THE ORDER.");
+
+        /* Closing the browser */
+        driverUtil.quitDriver(driver);
     }
 }
