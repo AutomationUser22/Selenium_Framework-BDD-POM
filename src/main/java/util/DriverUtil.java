1e8a4aa8276aa0b719bea6654c03d47186d0cb7d
Vinayak Mahajan <mahajavi@amazon.com>
2024-09-05 12:52:41 +0100

    Update the file to run the framework on both local and AWS Device Farm as per config

--- src/main/java/util/DriverUtil.java
+++ src/main/java/util/DriverUtil.java
@@ -5,24 +5,66 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import org.testng.asserts.SoftAssert;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.devicefarm.DeviceFarmClient;
+import software.amazon.awssdk.services.devicefarm.model.CreateTestGridUrlRequest;
+import software.amazon.awssdk.services.devicefarm.model.CreateTestGridUrlResponse;
+
+import java.net.URL;
 import java.time.Duration;
+import java.util.Objects;
 
 public class DriverUtil {
 
     private final PropReaderUtil propReaderUtil = new PropReaderUtil();
 
     public WebDriver driverSetUp(String browser) {
+
         WebDriver driver;
-        if (browser.equals("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        } else {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver(new ChromeOptions().addArguments(propReaderUtil.getChromeOptions()));
+        DesiredCapabilities desired_capabilities = new DesiredCapabilities();
+        URL testGridUrl = null;
+        SoftAssert softAssert = new SoftAssert();
+        String projectARN = propReaderUtil.getProjectARN();
+
+        if(propReaderUtil.getLocalOrCloud().equals("cloud")) {
+            try(DeviceFarmClient deviceFarmClient = DeviceFarmClient.builder().region(Region.US_WEST_2).build()){
+                CreateTestGridUrlRequest request = CreateTestGridUrlRequest.builder()
+                        .expiresInSeconds(300)
+                        .projectArn(projectARN)
+                        .build();
+                CreateTestGridUrlResponse response = deviceFarmClient.createTestGridUrl(request);
+                testGridUrl = new URL(response.url());
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            if (browser.equals("firefox")) {
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                softAssert.assertTrue(testGridUrl != null, "Test Grid URL is null");
+                driver = new RemoteWebDriver(testGridUrl, firefoxOptions);
+            } else {
+                ChromeOptions chromeOptions = new ChromeOptions().addArguments(propReaderUtil.getChromeOptions());
+                softAssert.assertTrue(testGridUrl != null, "Test Grid URL is null");
+                driver = new RemoteWebDriver(testGridUrl, chromeOptions);
+            }
+        }
+        else {
+            if (browser.equals("firefox")) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            } else {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(new ChromeOptions().addArguments(propReaderUtil.getChromeOptions()));
+            }
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(propReaderUtil.getDefaultImplicitWaitInSeconds()));
+        softAssert.assertAll();
         return driver;
     }
 
